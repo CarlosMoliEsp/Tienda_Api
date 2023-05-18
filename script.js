2cf0d3fc283133b5af85690c11e59b97ea55fb33
CarlosMoliEsp <carlosaldeire@gmail.com>
2023-05-18 08:55:48 +0200

    18/05
    No va la funcion del onclick

--- script.js
+++ script.js
@@ -1,58 +1,141 @@
-window.onload = () => {
+$(document).ready(function () {
     // alert("Pagina cargada");
     $('#onload').fadeOut();
     let json, json2, res;
     let categoriasjson;
     let nProductos = 0;
+
+    let carrito;
     inicio();
+    crearCarrito();
     const contenedor = document.querySelector('#contenedor');
+
+    // Carrito
+    async function crearCarrito() {
+        await fetch('https://fakestoreapi.com/carts', {
+            method: "POST",
+            body: JSON.stringify(
+                {
+                    userId: 5,
+                    date: 2020 - 02 - 03,
+                    products: []
+                }
+            )
+        })
+            .then(res => res.json())
+            .then((json) => {
+                carrito = json
+                console.log(carrito);
+
+            })
+
+        // console.log(carrito);
+    }
+    console.log(carrito);
+
 
     // Productos
     fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
         .then(json => json2 = json)
 
+        
     function muestraProductos(j) {
         i = nProductos;
         while (i < j.length && i < (nProductos + 8)) {
             // for (i = 0; i < j.length; i++) {
             contenedor.innerHTML += `
+            <div class="col-3">
                 <div class="card" style="width: 18rem;">    
                     <img class="card-img-top" src="${j[i].image}" alt="Card image cap" with="200px" height="300px">
                     <div class="card-body">
                         <h5 class="card-title">${j[i].title}</h5>
                         <p class="card-subtitle mb-2 text-muted">Precio: ${j[i].price}$</p>
+                        <h5 class="card-title">${j[i].category}</h5>
  
-                        <button class="btn btn-primary"> Ver </button>
-                        <a href="#" class="btn btn-primary">Comprar</a>
+
+                        <button onclick="verProducto(${j[i].id})" class="btn btn-primary"> Ver </button>
+                        <a id="comprar${i}" class="btn btn-primary">Comprar</a>
                     </div>
-                </div>  
+                </div> 
+            </div> 
             `;
+            console.log(j[i].id);
+            document.getElementById("comprar".j[i].id).addEventListener("click", function () {
+                console.log("Hila");
+            });
             i++;
         }
         nProductos = i;
+        // for (let index = 0; index <= j.length; index++) {
+        //     console.log(j[i].id);
+        //     document.getElementById("comprar".i).addEventListener("click", function () {
+        //         console.log("Hila");
+        //     });
+        // }
+        
 
     }
-
-    // Categorias
-    fetch('https://fakestoreapi.com/products/categories?sort=desc')
-        .then(res => res.json())
-        .then(json => categoriasjson = json)
-
-    function muestraCategorias(j) {
-        for (i = 0; i < j.length; i++) {
-            // $("#contenedor").append(j[i]);
-            $("#list").append('<li><h3>' + j[i] + '</h3></li>');
-        }
+    // No va
+    function verProducto(id) {
+        console.log(id);
     }
 
-    // Ver un solo Producto al Completo
-    // async function verProducto(id){
-    //     console.log(id);
+    // document.getElementById("comprar").addEventListener("click", function () {
+    //     console.log("Hila");
+    // });
+
+    // var compritas = document.getElementById("comprar");
+    // console.log(compritas);
+    // if (compritas) {
+    //     console.log(compritas);
+    //     compritas.addEventListener("click", function () {
+    //         console.log("Hila");
+    //     });
     // }
 
 
+    // Categorias
 
+    // Categoria Electronics
+    fetch('https://fakestoreapi.com/products/category/electronics')
+        .then(res => res.json())
+        .then(json => categoria1 = json)
+    function muestraCategoria(j) {
+        for (i = 0; i < j.length; i++) {
+            contenedor.innerHTML += `
+            <div class="col-2">
+                <div class="card" style="width: 15rem;">    
+                    <img class="card-img-top" src="${j[i].image}" alt="Card image cap" with="200px" height="300px">
+                    <div class="card-body">
+                        <h5 class="card-title">${j[i].title}</h5>
+                        <p class="card-subtitle mb-2 text-muted">Precio: ${j[i].price}$</p>
+                        <h5 class="card-title">${j[i].category}</h5>
+ 
+
+                        <button onclick="" class="btn btn-primary"> Ver </button>
+                        <a href="#" class="btn btn-primary">Comprar</a>
+                    </div>
+                </div> 
+            </div> 
+            `;
+        }
+    }
+    // Categoria Ropa Hombre
+    fetch("https://fakestoreapi.com/products/category/men's clothing")
+        .then(res => res.json())
+        .then(json => categoria2 = json)
+    
+    // Categoria Ropa Mujer
+    fetch("https://fakestoreapi.com/products/category/women's clothing")
+        .then(res => res.json())
+        .then(json => categoria3 = json)
+    
+
+    // Categoria Joyas
+    fetch('https://fakestoreapi.com/products/category/jewelery')
+        .then(res => res.json())
+        .then(json => categoria4 = json)
 
 
 
@@ -64,12 +147,12 @@ window.onload = () => {
         limpiarBody();
         document.getElementById("contenedor").innerHTML = "<h2>&nbsp;&nbsp;Estas en el Inicio de la tienda</h2>";
     }
+
     // Capa Productos
     document.getElementById("Productos").addEventListener("click", function () {
         limpiarBody();
         muestraProductos(json2);
     });
-
     // Scroll infinito
     $(window).scroll(function () {
         console.log($(window).scrollTop());
@@ -80,12 +163,34 @@ window.onload = () => {
         }
     });
 
-    // Capa Categorias
-    document.getElementById("Categorias").addEventListener("click", function () {
+    // Capa Categoria
+    //  Electronics
+    document.getElementById("Electronics").addEventListener("click", function () {
         limpiarBody();
         document.getElementById('categorias').style = 'display:block'
         // console.log(categoriasjson);
-        muestraCategorias(categoriasjson);
+        muestraCategoria(categoria1);
+    });
+    // Ropa Hombre
+    document.getElementById("Men").addEventListener("click", function () {
+        limpiarBody();
+        document.getElementById('categorias').style = 'display:block'
+        // console.log(categoriasjson);
+        muestraCategoria(categoria2);
+    });
+    // Ropa Mujer
+    document.getElementById("Women").addEventListener("click", function () {
+        limpiarBody();
+        document.getElementById('categorias').style = 'display:block'
+        // console.log(categoriasjson);
+        muestraCategoria(categoria3);
+    });
+    // Joyas
+    document.getElementById("Joyas").addEventListener("click", function () {
+        limpiarBody();
+        document.getElementById('categorias').style = 'display:block'
+        // console.log(categoriasjson);
+        muestraCategoria(categoria4);
     });
 
     // Pone en blanco el Body para cargar otra funcion
@@ -94,4 +199,4 @@ window.onload = () => {
         document.getElementById("categorias").style.display = "none";
     }
 
-}
+})
